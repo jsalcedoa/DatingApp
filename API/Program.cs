edaf9da631fab71a0d918bdb6f40fa9690440309
Jonattan Salcedo Alvarez <jssalcedo88@gmail.com>
2024-02-26 21:09:45 -0600

    End of section 7.

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +14,11 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseAuthentication();
-
 app.UseAuthorization();
 
 app.UseHttpsRedirection();
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -21,8 +21,7 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: () => this.router.navigateByUrl('/members'),
-      error: err => this.toastr.error(err.error)
+      next: () => this.router.navigateByUrl('/members')
     });
   };
 
